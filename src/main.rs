248d0b54c28675a5a75f9623b802a675f0fdf49f
felipesantos2 <santospinheiro627@gmail.com>
2026-01-17 19:10:56 -0300

    tipo primitivo inteiro e suas variações

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 fn main() {
-    // println!("Hello, world!"); 
+    // println!("Hello, world!");
+    let age: i32 = 22;
+    // i64 i31 i16 i8
 
-    
-    let age = 22;
-    
-    
-    println!("Minha idade é {age}")
-    
+    println!("Minha idade é: {age}");
+    println!("Minha idade é: {age}");
+
+    println!("Minha idade é: {age}");
+    println!("Minha idade é: {age}");
+
 }
